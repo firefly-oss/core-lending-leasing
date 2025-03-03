e4aec2e27e4adce52306d80161d3a0ba195dce7b
Andrés Contreras Guillén <andres.contreras@dworkers.ai>
2025-03-03 13:44:47 +0100

    Add DTOs for leasing module in core-lending services
    Introduced DTO classes for managing leasing-related functionalities, including agreements, assets, payments, schedules, end options, and service events. These classes provide structured representations for core leasing data, enabling filtering, serialization, and seamless usage across the application.

---
+++ core-lending-leasing-interfaces/src/main/java/com/catalis/core/lending/leasing/interfaces/dtos/event/v1/LeaseServiceEventDTO.java
@@ -0,0 +1,33 @@
+package com.catalis.core.lending.leasing.interfaces.dtos.event.v1;
+
+import com.catalis.common.core.filters.FilterableId;
+import com.catalis.core.lending.leasing.interfaces.enums.event.v1.EventTypeEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LeaseServiceEventDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long leaseServiceEventId;
+
+    @FilterableId
+    private Long leasingAssetId;
+
+    private LocalDate eventDate;
+    private EventTypeEnum eventType;
+    private BigDecimal cost;
+    private String note;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
